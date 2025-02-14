7efdce75bd92d968bf1eee79ccf467411a333487
Yael Castillo <yaelaram14@outlook.com>
2025-02-14 17:44:20 -0600

    Se inicio el proyecto Late Lab (Landingpage), se termino el componente Navbar y se realizaron cambios al Layout

---
+++ src/data/paginaIndex.ts
@@ -0,0 +1,8 @@
+export const paginaIndex = {
+  metaData: {
+    title: 'Late Lab',
+    description: 'Página principal del Laboratorio de Lenguaje y Análisis de Textos',
+    keywords: ['Late Lab', 'IIMAS', 'UNAM', 'Análisis de Textos', 'Laboratorio'],
+    image: '/favicon.svg',
+  }
+}
